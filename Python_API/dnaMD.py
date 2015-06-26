8c1d9cb47db5d5ee969e0a4c8b21067ce5d2b5dc
Rajendra Kumar <rjdkmr@gmail.com>
2015-06-26 12:08:34 +0200

    fixed installation bugs in cmake

--- Python_API/dnaMD.py
+++ Python_API/dnaMD.py
@@ -4,7 +4,7 @@
 # This file is part of do_x3dna
 #
 # Author: Rajendra Kumar
-# Copyright (C) 2014  Rajendra Kumar
+# Copyright (C) 2014, 2015  Rajendra Kumar
 #
 # do_x3dna uses 3DNA package (http://x3dna.org).
 # Please cite the original publication of the 3DNA package:
